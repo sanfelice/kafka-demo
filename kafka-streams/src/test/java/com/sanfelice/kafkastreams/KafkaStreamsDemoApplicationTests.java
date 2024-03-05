d37c9db4af2f88b85a082027e89522ffc95a8f60
Ricardo Sanfelice <ricardosanfelice@gmail.com>
2024-03-05 22:26:09 +0100

    Renamed project
    Renamed main module to follow repository name
    Fixed gradle warnings

--- kafka-streams/src/test/java/com/sanfelice/kafkastreams/KafkaStreamsDemoApplicationTests.java
+++ kafka-streams/src/test/java/com/sanfelice/kafkastreams/KafkaStreamsDemoApplicationTests.java
@@ -1,10 +1,17 @@
 package com.sanfelice.kafkastreams;
 
+import org.junit.jupiter.api.Test;
+
 //@SpringBootTest
 class KafkaStreamsDemoApplicationTests {
 
 //    @Test
 //    void contextLoads() {
 //    }
+
+    @Test
+    void test() {
+        assert true;
+    }
 
 }
